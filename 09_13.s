c153a3bd7e3a1577b0625b195b1138b9949856c3
asy <paul_040808@qq.com>
2024-09-14 18:52:11 +0800

    9-14

--- 09_13.s
+++ 09_13.s
@@ -1,126 +1,126 @@
-	.file	"09_13.c"
-	.option nopic
-	.attribute arch, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0"
-	.attribute unaligned_access, 0
-	.attribute stack_align, 16
-	.text
-	.globl	a
-	.section	.sbss,"aw",@nobits
-	.align	2
-	.type	a, @object
-	.size	a, 4
-a:
-	.zero	4
-	.globl	b
-	.align	2
-	.type	b, @object
-	.size	b, 4
-b:
-	.zero	4
-        .globl  c
-        .align  2
-        .type   c, @object
-        .size   c, 1
-c:
-        .zero   1
-	.text
-	.align	1
-	.globl	AD
-	.type	AD, @function
+.file   "09_13.c"
+.option nopic
+.attribute arch, "rv64i2p1_m2p0_a2p1_f2p2_d2p2_c2p0_zicsr2p0"
+.attribute unaligned_access, 0
+.attribute stack_align, 16
+.text
+.globl  AD
+.type   AD, @function
 AD:
-        mv a5,a1
-        mv a6,a2
-        add a5,a5,a6
-        sext.w a0,a5
-        ret
-        
-        .globl	SB
-	.type	SB, @function
-SB:
-        mv a5,a1
-        mv a6,a2
-        sub a5,a5,a6
-        sext.w a0,a5
-        ret
-        
-        .globl	ML
-	.type	ML, @function
+        mv     a5, a1
+        mv     a6, a2
+        addw   a5, a5, a6
+        sext.w	a5,a5
+        mv     a0,a5
+        jr ra
+        .size	AD, .-AD
 	
+	
+
+.globl  SB
+.type   SB, @function
+SB:
+        mv     a5, a1
+        mv     a6, a2
+        subw   a5, a5, a6
+        sext.w	a5,a5
+        mv     a0,a5
+        jr ra
+        .size	SB, .-SB
+	
+
+.globl  ML
+.type   ML, @function
+
 ML:
-        mv a5,a1
-        mv a6,a2
-        mul a5,a5,a6
-        sext.w a0,a5
-        ret   
+        mv     a5, a1
+        mv     a6, a2
+        mulw    a5, a5, a6
+        sext.w	a5,a5
+        mv     a0,a5
+        jr ra
+        
+        .size	ML, .-ML
+	
 
 .str1:
         .string "%d %d %c"
+        
+.str2:
+        .string "%d"
 
+.text
 
-        .globl	main
-	.type	main, @function
-	
+.globl  main
+.type   main, @function
+
 main:
-        addi sp,sp,-40
-        sd   ra,32(sp)
-	sd   s0,24(sp)
-	sd   s1,16(sp)
-	sd   s2,8(sp)
-	lui  a0,%hi(.str1)
-	lui  s0,%hi(a)
-	lui  s1,%hi(b)
-	lui  s2,%hi(c)
-	addi a0,a0,%lo(.str1)
-	addi a1,s0,%lo(a)
-	addi a2,s1,%lo(b)
-	addi a3,s2,%lo(c)
-	call __isoc99_scanf
-	lw   a1,%lo(a)(s0)
-	lw   a2,%lo(b)(s1)
-	lw   a3,%lo(c)(s2)
-	li   a4,43
-	li   a5,45
-	li   a6,32
-	beq  a3,a4,A
-	beq  a3,a5,S
-	beq  a3,a6,M
-	ld   ra,32(sp)
-	ld   s0,24(sp)
-	ld   s1,16(sp)
-	ld   s2,8(sp)
-	addi sp,sp,40
-	jr   ra
+        addi	sp,sp,-32
+	sd	ra,24(sp)
+	sd	s0,16(sp)
+	addi	s0,sp,32
+	addi	a3,s0,-25
+	addi	a4,s0,-24
+	addi	a5,s0,-20
+        mv     a1,a4
+        mv     a2,a5
+        mv     a3,a6
+        lui    a0, %hi(.str1)
+        addi   a0, a0, %lo(.str1)
+        call   __isoc99_scanf
+        lbu    a3,-25(s0)
+        li     a4, 43
+        bne    a3,a4,.A
+        lw     a0, -20(s0)
+        lw     a1, -24(s0)
+        call   AD
+        mv	a5,a0
+	mv	a1,a5
+	lui	a5,%hi(.str2)
+	addi	a0,a5,%lo(.str2)
+	call	printf
+	j      .end
+	
+        
+.A:
+        lbu    a3,-25(s0)
+        li     a4, 45
+        bne    a3,a4,.S
+        lw     a0, -20(s0)
+        lw     a1, -24(s0)
+        call   SB
+        mv	a5,a0
+	mv	a1,a5
+	lui	a5,%hi(.str2)
+	addi	a0,a5,%lo(.str2)
+	call	printf
+	j      .end
 	
 
-A:
-        call AD
-        call printf
-        ld   ra,32(sp)
-	ld   s0,24(sp)
-	ld   s1,16(sp)
-	ld   s2,8(sp)
-	addi sp,sp,40
-	jr   ra
+.S:
+        lbu    a3,-25(s0)
+        li     a4, 32
+        bne    a3,a4,.end
+        lw     a0, -20(s0)
+        lw     a1, -24(s0)
+        call   ML
+        mv	a5,a0
+	mv	a1,a5
+	lui	a5,%hi(.str2)
+	addi	a0,a5,%lo(.str2)
+	call	printf
+	j      .end
+	
 
-S:
-        call SB
-        call printf
-        ld   ra,32(sp)
-	ld   s0,24(sp)
-	ld   s1,16(sp)
-	ld   s2,8(sp)
-	addi sp,sp,40
-	jr   ra
 
-M:
-        call ML
-        call printf
-        ld   ra,32(sp)
-	ld   s0,24(sp)
-	ld   s1,16(sp)
-	ld   s2,8(sp)
-	addi sp,sp,40
-	jr   ra
+.end:
+        li	a5,0
+	mv	a0,a5
+	ld	ra,24(sp)
+	ld	s0,16(sp)
+	addi	sp,sp,32
+	jr	ra
+       
 	
 	
 	
